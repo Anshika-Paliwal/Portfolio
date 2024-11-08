58edbda580e4d90b132b2e9f68000603967de65a
Anshika-Paliwal <paliwal.anshika2028@gmail.com>
2024-11-09 01:16:28 +0530

    Insta account link rectified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -52,7 +52,7 @@ const Hero = () => {
             </a>
 
             <a
-              href="https://www.instagram.com/___anshika.__/"
+              href="https://www.instagram.com/anshikaapaliwal/"
               target="_blank"
               rel="noopener noreferrer"
             >
